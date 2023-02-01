e70b746899cc3d9615e98448bbe6156bf2c12553
bwilkins96 <ben.jr.speed@gmail.com>
2023-02-01 12:36:11 -0500

    saving and screen cover

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 let clickCount = 0
 let clicker = document.getElementById('main')
 
-const handleClick = () => {
-    clickCount++;
-
-    console.log('hey!')
-
+const handleClick = (increment=true) => {
+    if (increment) {
+        clickCount++;
+    }
+    
     if (clickCount === 1) {
         clicker.innerText = 'You have clicked 1 time!'
     } else {
         clicker.innerText = `You have clicked ${clickCount} times!`
-    }  
+    }
+    
+    save()
+}
+
+const save = () => {
+    localStorage.setItem('clickCount', clickCount)
+}
+
+const restore = () => {
+    clickCount = Number(localStorage.getItem('clickCount'))
+    handleClick(false)
 }
 
 const run = () => {
     document.addEventListener('click', handleClick)
+
+    if (localStorage.getItem('clickCount')) {
+        restore()
+    }
 }
 
 run()
